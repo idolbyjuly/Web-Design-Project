210348c049600c86250f21c2b809ad00defe033e
JulyJ <idolbyjuly@gmail.com>
2018-10-02 14:14:36 -0700

    First Web Design
    the easy web boring game, use A.S.W.D to move box wherever you want to move, that is all.

---
+++ web_game/key.js
@@ -0,0 +1,24 @@
+var key={
+	a:false,
+	d:false,
+	s:false,
+	w:false
+};
+
+window.addEventListener('keydown',function(event){
+	switch(event.which || event.keyCode){
+		case 65: key.a=true;break;
+		case 68: key.d=true;break;
+		case 83: key.s=true;break;
+		case 87: key.w=true;break;
+	}
+});
+
+window.addEventListener('keyup',function(event){
+	switch(event.which || event.keyCode){
+		case 65: key.a=false;break;
+		case 68: key.d=false;break;
+		case 83: key.s=false;break;
+		case 87: key.w=false;break;
+	}
+});
---
+++ web_game/web_game.js
@@ -0,0 +1,37 @@
+var canvas = document.getElementById("screen"); 
+var context = canvas.getContext("2d"); 
+
+var x = 50;
+var y = 50;
+
+function init(){
+	context.fillStyle = "skyblue";
+}
+
+function move_box(dx,dy){
+	x+=dx;
+	y+=dy;
+}
+
+function update(){
+	if(key.a){move_box(-5,0);}
+	if(key.d){move_box(5,0);}
+	if(key.w){move_box(0,-5);}
+	if(key.s){move_box(0,5);}
+}
+
+function draw(){
+	context.clearRect(0,0, canvas.width,canvas.height);
+	context.fillRect(x,y,50,50);
+	 
+}
+
+function main(){
+	update();
+	draw();
+}
+
+window.onload = function(){
+	init();
+	setInterval(main,1000/80);
+};
